795a714ee9cf779fe4eb71db4013282f04905f92
Lucas Rosa <moeda.60@gmail.com>
2020-03-16 14:09:26 -0300

    format price

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import NumberFormat from 'react-number-format';
 
-import {formatPrice} from '../../util/format';
 import {Container, Image, Name, Price, Button, ButtonText} from './styles';
 
 const Card = ({data}) => {
   const {title, price, image} = data;
 
-  // formatPrice(price).then(p => console.log(p));
-
-  // console.log(image);
-
   return (
     <Container>
       <Image source={{uri: image}} />
       <Name> {title} </Name>
-      <Price> {price} </Price>
+      <NumberFormat
+        value={price}
+        displayType="text"
+        thousandSeparator="."
+        prefix="R$"
+        decimalSeparator=","
+        renderText={formattedText => <Price>{formattedText}</Price>}
+      />
       <Button>
         <Icon name="shopping-cart" size={20} color="#FFF" />
         <ButtonText>Adicionar ao carrinho</ButtonText>
--- src/util/format.js
+++
@@ -1,6 +0,0 @@
-import Intl from 'react-native-intl';
-
-export const {format: formatPrice} = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
-});
