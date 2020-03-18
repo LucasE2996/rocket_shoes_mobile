d6d04e85b2d42827ed03a71b4d1f996f6cbe78d9
LucasE2996 <moeda.60@gmail.com>
2020-03-17 23:07:21 -0300

    configure redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar} from 'react-native';
+import {Provider} from 'react-redux';
+
 import Routes from './routes';
+import store from './store';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#191920" />
       <Routes />
-    </>
+    </Provider>
   );
 };
 
